1548b89ae41666e0a733b825962a2548359b10ab
joseapp25 <joseapp25@gmail.com>
2026-03-20 11:04:25 -0300

    Changes

--- src/views/Lessons.vue
+++ src/views/Lessons.vue
@@ -8,23 +8,6 @@
         <nav>
             <router-link to="/">Home</router-link>
         </nav>
-
-        <div class="text-email">
-
-            <p style="margin-bottom: 32px;">Saudações,</p>
-
-            <p style="margin-bottom: 8px;">Clique neste link para redefinir a senha de login no app <span style="font-weight: 900">Trainer</span> do <span style="color: #2269D3; font-weight: 900;">Phoneticando</span> com sua conta %EMAIL%.</p>
-
-            <p style="margin-bottom: 16px;"><a href='%LINK%'>%LINK%</a></p>
-
-            <p style="margin-bottom: 32px;"><span style="font-weight: 900;">Observação:</span> Desconidere este email caso não tenha solicitado a redefinição da sua senha.</p>
-
-            <p>Att,</p>
-
-            <p>Equipe do <span style="color: #2269D3; font-weight: 900;">Phoneticando</span>.</p>
-
-        </div>
-
     </main>
     <footer>
         <PhoneticandoFooter />
@@ -39,8 +22,5 @@
 </script>
 
 <style scoped>
-.text-email{
-    margin-top: 32px;
-}
 
 </style>
